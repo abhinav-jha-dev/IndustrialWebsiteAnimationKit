794c78724ce398654b22438460d3295b88b73ed9
Abhinav <abhinav.jha58@gmail.com>
2017-10-02 15:36:59 +0530

    location test

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
-    var base_url = window.location;
+    var base_url = window.location.href;
     $("#shipbtn").click(function () {
         window.location.href = base_url + "mithap/ship.html";
     });
